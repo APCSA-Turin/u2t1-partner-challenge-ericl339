85766eb3eee75166892ea8b9892d8dcbf6c1f8b7
ericl339 <148896243+ericl339@users.noreply.github.com>
2024-09-24 15:57:38 +0000

    calc

--- src/main/java/com/example/project/CalculatorRunner.java
+++ src/main/java/com/example/project/CalculatorRunner.java
@@ -5,8 +5,22 @@ public class CalculatorRunner {
 
         //test your program here
         //1. create an instance of the calculator class
-
+        Calculator calc = new Calculator("Ti84");
         //2. call any methods of that class to test
+        double operation = calc.performOperation("+", -5, 6);
+        System.out.println(operation);
+        
+        boolean trueFalse = calc.divisibleBy(-5, 6);
+        System.out.println(trueFalse);
 
+        String coords = calc.coordinatePair(-5, 6);
+        System.out.println(coords);
+        
+        int absValue = calc.absoluteValue(-5, 6);
+        System.out.println(absValue);
+
+        String information = calc.info();
+        System.out.println(information);
+        
     }
 }
